8c4e119a37e14739383ef026f95e641a340ee8b4
bacbpl-2026 <subhasishsinghabacbpl@gmail.com>
2026-01-23 17:57:42 +0530

    watcho update

--- services/watcho.service.js
+++ services/watcho.service.js
@@ -1,24 +1,35 @@
 const axios = require("axios");
-const { BASE_URL, SECRET_KEY, DEFAULT_PAYLOAD } = require("../config/watcho.config");
+const { BASE_URL, SECRET_KEY } = require("../config/watcho.config");
 const cryptoUtil = require("../utils/watchoCrypto");
 
-exports.getSubscriptionPlans = async () => {
-  // 1 Encrypt request
-  const encrypted = cryptoUtil.encrypt(DEFAULT_PAYLOAD, SECRET_KEY);
+const AUTH_USER = "160";
+const AUTH_PASS = "bH@reer!$#2345";
+const ENTITY_ID = "10950334";
 
-  // 2 Call Watcho API
+exports.getSubscriptionPlans = async () => {
+  const payload = {
+    OTTSubscriberID: "-1",
+    UserID: "10950334",
+    UserType: "DS",
+    Source: "IS"
+  };
+
+  const encrypted = cryptoUtil.encrypt(payload, SECRET_KEY);
+
   const response = await axios.post(
     `${BASE_URL}/SubscriptionPlanDetails`,
     { InputData: encrypted },
     {
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "EntityID": ENTITY_ID,
+        "Authorization":
+          "Basic " +
+          Buffer.from(`${AUTH_USER}:${AUTH_PASS}`).toString("base64")
       }
     }
   );
 
-  // 3️⃣ Decrypt response
-  const decryptedResponse = cryptoUtil.decrypt(response.data, SECRET_KEY);
-
-  return decryptedResponse;
+  // Watcho sends encrypted response as STRING
+  return cryptoUtil.decrypt(response.data, SECRET_KEY);
 };
