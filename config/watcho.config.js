78d06963d79a9c4323fca05338b0048e00e8d0db
bacbpl-2026 <subhasishsinghabacbpl@gmail.com>
2026-01-23 17:46:28 +0530

    watcho api first update

--- config/watcho.config.js
+++ config/watcho.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   BASE_URL: "https://publicapis.dishtv.in/api/WatchoOne",
-  SECRET_KEY: "W#e0!2s5v8y/B?E(H+Kb",
+  SECRET_KEY: "W@!0$2s5v8y/B?E(H+Kb",
   DEFAULT_PAYLOAD: {
-    OTTSubscriberID: "-1",
-    UserID: "10950334",
+    OTTSubscriberID: -1,
+    UserID: 10950334,
     UserType: "DS",
     Source: "IS"
   }
