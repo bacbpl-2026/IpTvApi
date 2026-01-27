d45394035161056763f9d78836da925328ab5384
bacbpl-2026 <subhasishsinghabacbpl@gmail.com>
2026-01-27 18:16:02 +0530

    login auth middleware

--- utils/watchoCrypto.js
+++ utils/watchoCrypto.js
@@ -56,6 +56,44 @@
 // };
 const crypto = require("crypto");
 
+exports.watchoEncrypt = (text, secret) => {
+  const key = crypto.createHash("sha256")
+    .update(secret)
+    .digest()
+    .slice(0, 24); // AES-192 key
+
+  const iv = Buffer.alloc(16, 0);
+
+  const cipher = crypto.createCipheriv("aes-192-cbc", key, iv);
+  let encrypted = cipher.update(text, "utf8", "base64");
+  encrypted += cipher.final("base64");
+
+  return encodeURIComponent(encrypted);
+};
+
+exports.watchoDecrypt = (encryptedText, secret) => {
+  const key = crypto
+    .createHash("sha256")
+    .update(secret)
+    .digest()
+    .slice(0, 24); // AES-192 key
+
+  const iv = Buffer.alloc(16, 0); // same IV as encryption
+
+  // reverse URL encoding
+  const decoded = decodeURIComponent(encryptedText);
+
+  const decipher = crypto.createDecipheriv("aes-192-cbc", key, iv);
+  let decrypted = decipher.update(decoded, "base64", "utf8");
+  decrypted += decipher.final("utf8");
+
+  return decrypted;
+};
+
+// const plainText = "160|bH@reer!$#2345|10950334";
+// const encryptedValue = encrypt(plainText);
+// console.log(encryptedValue);
+
 function md5Key(secret) {
   return crypto.createHash("md5").update(secret).digest();
 }
